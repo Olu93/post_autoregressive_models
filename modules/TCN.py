2f0a9aeba22113de0b33dea55e06a1ed3558a557
Olubuntu <o.hundogan@gmail.com>
2021-10-21 14:11:59 +0200

    Semi working version

--- modules/TCN.py
+++ modules/TCN.py
@@ -10,24 +10,22 @@ class PureTCN(nn.Module):
     def __init__(self, num_inputs, level_embedding_sizes, kernel_size=2, stride=1):
         super(PureTCN, self).__init__()
         layers = []
-        all_level_embedding_sizes = [num_inputs] + list(level_embedding_sizes)
-        n_levels = len(all_level_embedding_sizes)
+        self.n_inputs = num_inputs
+        self.n_levels = len(level_embedding_sizes)
 
-        for i in range(n_levels - 1):
+        for i in range(self.n_levels - 1):
             dilation = 2**(i)
-            in_emb_size = all_level_embedding_sizes[i]
-            out_emb_size = all_level_embedding_sizes[i + 1]
+            in_emb_size = level_embedding_sizes[i]
+            out_emb_size = level_embedding_sizes[i + 1]
             layers.append(self._add_tcn_module(kernel_size, dilation, stride, in_emb_size, out_emb_size))
-            
 
         self.network = nn.Sequential(*layers)
 
     def _add_tcn_module(self, kernel_size, dilation, stride, in_emb_size, out_emb_size):
-        print("params ", in_emb_size, out_emb_size, kernel_size, stride, dilation)
+        # print("params ", in_emb_size, out_emb_size, kernel_size, stride, dilation)
         return TemporalBlockModule(in_emb_size, out_emb_size, kernel_size, stride, dilation)
 
     def forward(self, x):
-        print(x.shape)
         return self.network(x)
 
 
@@ -45,12 +43,12 @@ class ResidualTCN(PureTCN):
 if __name__ == '__main__':
     batch_size = 3
     n_channels = 5
-    len_sequence = 10
+    len_sequence = 30
     kernel_size = 2
     input = torch.from_numpy(
         np.arange(batch_size * n_channels * len_sequence).reshape((batch_size, n_channels, len_sequence))).float()
     print(input.shape)
-    ss = (n_channels, 6, 7, 8)
-    module = PureTCN(num_inputs=batch_size, level_embedding_sizes=ss, kernel_size=kernel_size)
+    ss = (n_channels, 6, 7, 8, 3)
+    module = ResidualTCN(num_inputs=batch_size, level_embedding_sizes=ss, kernel_size=kernel_size)
     out = module(input)
     print(out.shape)
